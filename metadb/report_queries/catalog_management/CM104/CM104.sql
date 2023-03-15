3af85f0d920d18699250a696307c4350fbb0ba8f
Erin Block <85253480+blocke1@users.noreply.github.com>
2023-03-15 13:03:38 -0600

    Update CM104.sql
    Hi @sabetiv - Index Data is still looking into whether inventory.creation_date is correct or not, but I would like your advice on the parentheses. Currently, they seem to only be querying and returning results from the 994. We need to be querying both the 994 and 998, but I seem to be at the point where I'm running my head into a wall. Thanks for any help!

--- metadb/report_queries/catalog_management/CM104/CM104.sql
+++ metadb/report_queries/catalog_management/CM104/CM104.sql
@@ -6,3 +6,18 @@ TABLES
 
 FILTERS FOR USER TO SELECT:
 */
+SELECT * FROM folio_source_record.marc__t as marc
+LEFT JOIN folio_inventory.instance as inventory
+  ON inventory.id = marc.instance_id
+  where marc.field= '994' and marc.sf = 'b' and marc.content != 'UCX'
+  and (marc.field = '998' and marc.sf = 'c' and marc."content" != 'f'
+  or marc."content" != 'w'
+  or marc."content" != 's'
+  or marc."content" != 'y'
+  or marc."content" != '-'
+  or marc."content" != 'b'
+  or marc."content" != 'o'
+  or marc."content" != 'n'
+  or marc."content" != 'g')
+	and inventory.creation_date >= '2023-01-01 00:00:00.000'
+	and inventory.creation_date < '2023-01-31 00:00:00.000'

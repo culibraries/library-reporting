3038cf51267d900ffcff7066860a13daca209637
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2023-04-21 09:26:13 -0600

    Update MM100.sql
    First draft of MM100

--- metadb/report_queries/materials_management/MM100/MM100.sql
+++ metadb/report_queries/materials_management/MM100/MM100.sql
@@ -1,8 +1,10 @@
-/** Documentation of Materials Management: Google Books Individual Manifests 
-
-DERIVED TABLES
-
-TABLES
-
-FILTERS FOR USER TO SELECT:
-*/
+--MM100 Google Books Cart Manifests Report
+--This reports returns a list of titles that are checked out to google carts. 
+--NEED TO ADD FORMER IDENTIFIER AFTER NEXT DRY RUN
+SELECT
+u.barcode,
+i.barcode
+FROM folio_circulation.loan__t__ l
+LEFT JOIN folio_inventory.item__t i ON l.item_id = i.id 
+LEFT JOIN folio_users.users__t u ON l.user_id = u.id 
+WHERE u.barcode LIKE 'Google%'

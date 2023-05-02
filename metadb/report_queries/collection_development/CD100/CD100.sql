4f6d4f3ae243e3b845f4f3a2c192155ad86e0545
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2023-05-02 09:05:51 -0600

    Update CD100.sql
    Cleaned up notes and added ISBN temporary table to pull in only ISBNS.

--- metadb/report_queries/collection_development/CD100/CD100.sql
+++ metadb/report_queries/collection_development/CD100/CD100.sql
@@ -1,36 +1,23 @@
-/*CD100: Shelf List
- * This reports returns a list of titles and item and biliographic information. 
- * 
- *These filters still need to be added: 
- * Item Create Date
- * ISBNS from folio_derived.identifiers (right now, using a local table in arag3385 schema)
- * 
- * Notes:
- * The query takes a long time to run if the pubisher filter is added
- * in parameters. It runs at a much faster time when I added it in
- * the Where clause. is it better to create
- * 
- * AND 
- * publisher LIKE '%Random House%'
- * '%Random House%' ::VARCHAR AS publisher_filter
- * AND 
- * (publisher LIKE (SELECT status_filter FROM parameters) OR (SELECT status_filter FROM parameters) = '')
- * 
- * when I enter subject into the WHERE statement, i get a 'column does not exist' error
- * 
- * Filters: Enter values for each filter between the 'green' single quotations. 
- * Enter filters for publisher below. 
- */
+--CD100: Shelf List
+--This reports returns a list of titles and item and biliographic information. 
+--These filters still need to be added: Item Create Date
+--
 WITH parameters AS (
 	SELECT 
-		'' ::VARCHAR AS effective_location_filter, /* 
-	Locations: https://docs.google.com/spreadsheets/d/1MMGloxcRGcvQ6EpY9tvu8Gi-pOTYmzGZklEqp2_WQMg/edit#gid=0
-		*/
-		'' ::VARCHAR AS status_filter /* 
-								Available, Restricted, Withdrawn, Long missing,
-								In process, Unavailable, In process (non-requestable), 
-								Lost and paid, In transit, Missing, Checked out, Aged to lost, 
-								Awaiting pickup, Claimed returned, On order, Declared lost*/
+		'' ::VARCHAR AS effective_location_filter,
+			--Locations: https://docs.google.com/spreadsheets/d/1MMGloxcRGcvQ6EpY9tvu8Gi-pOTYmzGZklEqp2_WQMg/edit#gid=0
+		'' ::VARCHAR AS status_filter -- Available, Restricted, Withdrawn, Long missing, In process, 
+								--Unavailable, In process (non-requestable), 
+								--Lost and paid, In transit, Missing, Checked out, Aged to lost, 
+								--Awaiting pickup, Claimed returned, On order, Declared lost*/
+),
+ISBN AS (
+SELECT
+ii.instance_id AS instance_id,
+string_agg (DISTINCT ii.identifier,' | ') AS ISBN
+FROM folio_derived.instance_identifiers AS ii
+WHERE ii.identifier_type_name = 'ISBN'
+GROUP BY instance_id
 )
 SELECT
 	i.item_id,
@@ -48,7 +35,7 @@ SELECT
 	i.discovery_suppress as item_suppressed,
 	link.title,
 	string_agg (distinct ic.contributor_name,' | ') as author,
-	string_agg (DISTINCT ii.identifier,' | ') AS ISBN,
+	ISBN.ISBN,
 	string_agg (DISTINCT ip.publisher, ' | ') AS publisher, 
 	string_agg (DISTINCT ip.date_of_publication,' | ') AS date_publication,
 	string_agg (DISTINCT sub.subjects, ' | ') AS inst_subject,
@@ -62,18 +49,15 @@ FROM
 	LEFT JOIN folio_derived.loans_renewal_count AS loan_renewals ON i.item_id = loan_renewals.item_id
 	LEFT JOIN folio_derived.items_holdings_instances AS link ON i.item_id = link.item_id
 	LEFT JOIN folio_derived.instance_ext AS inst ON link.instance_id = inst.instance_id 
+	LEFT JOIN ISBN ON ISBN.instance_id = inst.instance_id 
 	LEFT JOIN folio_derived.instance_contributors AS ic ON inst.instance_id = ic.instance_id 
-	LEFT JOIN arag3385.instance_isbns AS ii ON inst.instance_id = ii.instance_id 
 	LEFT JOIN folio_derived.instance_publication ip ON inst.instance_id = ip.instance_id 
 	LEFT JOIN folio_derived.instance_subjects AS sub ON inst.instance_id = sub.instance_id 
 WHERE
-/*ENTER FILTER FOR PUBLISHER, AUTHOR, SUBJECTS. DELETE IF NOT FILTERING. 
-*/
-publisher LIKE '%Bloomsbury%'
-/*
- * APPLIES FILTERS FROM THE TOP OF THE QUERY
- */
-AND
+--Enter publisher name in within gree quotation marks (''). Enter "--"" before "publisher" if not using this filter.
+publisher LIKE '%Bloomsbury%' AND
+--
+--APPLIES FILTERS FROM THE TOP OF THE QUERY
 (i.effective_location_name  = (SELECT effective_location_filter FROM parameters) OR (SELECT effective_location_filter FROM parameters) = '') 
 AND 
 (status_name = (SELECT status_filter FROM parameters) OR (SELECT status_filter FROM parameters) = '')
@@ -94,6 +78,7 @@ i.enumeration,
 i.material_type_name,
 item_suppressed,
 link.title,
+ISBN.ISBN,
 loan_item_status,
 loans_items.loan_due_date,
 loans_items.loan_return_date,

173cd65609fa5007acdaac2197248dcd1d6f3328
Erin Block <85253480+blocke1@users.noreply.github.com>
2023-06-05 07:36:25 -0600

    Update CM108.sql

--- metadb/report_queries/catalog_management/CM108/CM108.sql
+++ metadb/report_queries/catalog_management/CM108/CM108.sql
@@ -6,3 +6,20 @@ TABLES
 
 FILTERS FOR USER TO SELECT:
 */
+WITH in_scope AS (
+SELECT *
+FROM
+folio_source_record.marc__t as marc
+WHERE marc.field = '998'
+AND marc.sf = 'c')
+SELECT
+inst.instance_hrid,
+inst.staff_suppress,
+holdings.permanent_location_name,
+in_scope.instance_id
+FROM folio_derived.instance_ext as inst
+LEFT JOIN folio_derived.holdings_ext as holdings ON holdings.instance_id = inst.instance_id
+LEFT JOIN in_scope ON in_scope.instance_id = inst.instance_id
+WHERE staff_suppress != TRUE
+AND holdings.permanent_location_name not like '%ILL%'
+AND holdings.permanent_location_name NOT LIKE '%Law%'

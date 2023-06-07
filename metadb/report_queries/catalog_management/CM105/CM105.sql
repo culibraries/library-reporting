05f26bfa2b8b5f5be725883b30523c46f141bfcd
Erin Block <85253480+blocke1@users.noreply.github.com>
2023-06-07 14:16:36 -0600

    Update CM105.sql
    Added SQL for Occam's Reader query. Working if only HRIDs are needed.

--- metadb/report_queries/catalog_management/CM105/CM105.sql
+++ metadb/report_queries/catalog_management/CM105/CM105.sql
@@ -6,3 +6,8 @@ TABLES
 
 FILTERS FOR USER TO SELECT:
 */
+SELECT instance_id FROM folio_source_record.marc__t AS marc
+WHERE (marc.field = '035' and marc.sf = 'a' and marc.content like '%spr%')
+INTERSECT
+(SELECT instance_id FROM folio_source_record.marc__t AS marc2
+where (marc2.field = '998' and marc2.sf = 'e' and marc2.content != 'n'));

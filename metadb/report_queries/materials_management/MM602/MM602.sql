9edf5834c36f0b96051e4084ae32fe7143a7404a
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2023-06-15 09:29:47 -0600

    Update MM602.sql
    Adding query for MM602

--- metadb/report_queries/materials_management/MM602/MM602.sql
+++ metadb/report_queries/materials_management/MM602/MM602.sql
@@ -1,8 +1,20 @@
-/** Documentation of <track name here>: <report name here> 
-
-DERIVED TABLES
-
-TABLES
-
-FILTERS FOR USER TO SELECT:
-*/
+--MM602: Norlin Awaiting Delivery Report
+--Returns a list of all items in Norlin Library locations with a status of 'Awaiting Delivery'
+SELECT 
+loc.name AS LOCATION,
+i.barcode, 
+h.call_number,
+i.volume,
+inst.index_title,
+jsonb_extract_path_text(ij.jsonb, 'status', 'name') AS status_name
+FROM folio_inventory.item__t AS i
+LEFT JOIN folio_inventory.holdings_record__t AS h ON i.holdings_record_id = h.id
+LEFT JOIN folio_inventory.instance__t AS inst ON h.instance_id = inst.id
+LEFT JOIN folio_inventory.location__t AS loc ON loc.id = i.effective_location_id
+LEFT JOIN folio_inventory.loclibrary__t AS lib ON lib.id = loc.library_id
+LEFT JOIN folio_inventory.item AS ij ON ij.id = i.id
+WHERE 
+jsonb_extract_path_text(ij.jsonb, 'status', 'name') = 'Awaiting Delivery'
+AND lib.name = 'Norlin'
+ORDER BY loc.name, h.call_number
+;

6dc17c054c922e16cc00979b00b537a2195d4baf
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2023-06-15 10:37:22 -0600

    Update MM800.sql
    added query for mm800

--- metadb/report_queries/materials_management/MM800/MM800.sql
+++ metadb/report_queries/materials_management/MM800/MM800.sql
@@ -1,8 +1,18 @@
-/** Documentation of <track name here>: <report name here> 
-
-DERIVED TABLES
-
-TABLES
-
-FILTERS FOR USER TO SELECT:
-*/
+--MM800 Online Item Inventory
+--Returns a list of all items that that have a location mapped to the Online Library. 
+SELECT 
+loc.name AS LOCATION,
+i.barcode, 
+h.call_number,
+i.volume,
+inst.index_title,
+jsonb_extract_path_text(ij.jsonb, 'status', 'name') AS status_name
+FROM folio_inventory.item__t AS i
+LEFT JOIN folio_inventory.holdings_record__t AS h ON i.holdings_record_id = h.id
+LEFT JOIN folio_inventory.instance__t AS inst ON h.instance_id = inst.id
+LEFT JOIN folio_inventory.location__t AS loc ON loc.id = i.effective_location_id
+LEFT JOIN folio_inventory.loclibrary__t AS lib ON lib.id = loc.library_id
+LEFT JOIN folio_inventory.item AS ij ON ij.id = i.id
+AND lib.name = 'Online'
+ORDER BY loc.name, h.call_number
+;

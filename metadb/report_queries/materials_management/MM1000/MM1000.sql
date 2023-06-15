4203e5587784a9714680447fe0653ca45562fe5f
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2023-06-15 09:22:15 -0600

    Update MM1000.sql
    updated query for mm1000

--- metadb/report_queries/materials_management/MM1000/MM1000.sql
+++ metadb/report_queries/materials_management/MM1000/MM1000.sql
@@ -1,10 +1,13 @@
---MM100 Google Books Cart Manifests Report
+--MM9000: Google Book Carts Manifest Report
 --This reports returns a list of titles that are checked out to google carts. 
---NEED TO ADD FORMER IDENTIFIER AFTER NEXT DRY RUN
 SELECT
-u.barcode,
-i.barcode
+u.barcode AS cart,
+i.barcode,
+former_barcode.jsonb #>> '{}' AS former_barcode
 FROM folio_circulation.loan__t__ l
 LEFT JOIN folio_inventory.item__t i ON l.item_id = i.id 
+LEFT JOIN folio_inventory.item AS ij ON ij.id = i.id 
+CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(ij.jsonb, 'formerIds')) WITH ORDINALITY AS former_barcode (jsonb)
 LEFT JOIN folio_users.users__t u ON l.user_id = u.id 
 WHERE u.barcode LIKE 'Google%'
+;

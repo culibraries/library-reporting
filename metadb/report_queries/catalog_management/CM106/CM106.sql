c92d3a9ed2cd6da1e3e12a7ade90c84f6f203818
Erin Block <85253480+blocke1@users.noreply.github.com>
2023-08-03 10:15:09 -0600

    Update CM106.sql
    Query for RAPID monographs.

--- metadb/report_queries/catalog_management/CM106/CM106.sql
+++ metadb/report_queries/catalog_management/CM106/CM106.sql
@@ -6,3 +6,21 @@ TABLES
 
 FILTERS FOR USER TO SELECT:
 */
+SELECT 
+inst.hrid,
+count(item.id)
+FROM
+folio_inventory.item__t AS item
+LEFT JOIN folio_inventory.holdings_record__t AS holdings ON holdings.id = item.holdings_record_id
+LEFT JOIN folio_inventory.instance__t AS inst ON inst.id = holdings.instance_id
+LEFT JOIN folio_derived.instance_statistical_codes AS stat ON stat.instance_id = inst.id
+LEFT JOIN folio_derived.instance_formats AS format ON format.instance_id = inst.id
+LEFT JOIN folio_inventory.location__t AS loc ON loc.id = holdings.effective_location_id
+where loc.name not like '%Rare Books Collection%'
+and loc.name not like '%Archives%'
+AND loc.name NOT LIKE '%Law%'
+AND stat.statistical_code_name = 'Book, print (books)'
+AND item.barcode != '[NULL]'
+AND format.instance_format_name = 'unmediated -- volume'
+AND inst.discovery_suppress = FALSE
+GROUP BY inst.hrid ORDER BY count(item.id) DESC

10b254dc69995eba33476796ac12278c6547acd9
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2023-10-18 18:41:33 -0600

    Update PS103.sql
    updates sql for PS103

--- metadb/report_queries/patron_services/PS103/PS103.sql
+++ metadb/report_queries/patron_services/PS103/PS103.sql
@@ -1,39 +1,51 @@
---PS103: Patron Billing Report
---This report provide information about all fee/fine actions during a specific amount of time. 
+--PS103: Patron Billing Report - Lost Item Fee
 WITH primary_address AS (
 SELECT *
 FROM folio_derived.users_addresses AS ua
 WHERE ua.is_primary_address = TRUE)
-SELECT
+SELECT 
 ffa.SOURCE,
+a.id AS accounts_id,
 ffa.id AS action_id,
-ffa.date_action AS Action_Date,
-ffa.user_id,
-u.jsonb ->> 'barcode' AS user_barcode,
-u.jsonb ->> 'username' AS user_username,
-concat(u.jsonb -> 'personal' ->> 'firstName',' ',u.jsonb -> 'personal' ->> 'lastName') AS user_name,
-a.fee_fine_type AS account_fee_fine_type,
-a.amount AS account_original_amount,
-ffa.type_action AS feefine_action,
-ffa.amount_action AS fee_fine_action_amount,
-ffa.payment_method AS payment_method,
-a.remaining AS accounts_remaining,
-ffa.COMMENTS,
-pg.GROUP AS patron_group,
-pa.address_line_1,
-pa.address_region,
-pa.address_postal_code,
+ffa.date_action::date AS date_action,
 a.LOCATION AS item_location,
+u.jsonb ->> 'barcode' AS user_barcode,
+u.jsonb ->> 'externalSystemId' AS user_externalSystemId,
+concat(u.jsonb -> 'personal' ->> 'lastName',', ',u.jsonb -> 'personal' ->> 'firstName') AS user_name,
+pa.address_line_1 as "AddressLine1",
+CONCAT_WS(' ', CONCAT_WS(', ',pa.address_city, pa.address_region), pa.address_postal_code) as "AddressLine2",
+pg.GROUP AS patron_group,
 a.barcode AS item_barcode,
 a.title AS item_title,
-a.call_number AS item_call_number
-FROM folio_feesfines.feefineactions__t__ AS ffa
+a.call_number AS item_call_number,
+sum(a.amount) AS "Amount",
+0 AS "Amount 2",
+0 AS "Amount 3",
+sum(a.amount) AS "Amt Total",
+a.remaining AS accounts_remaining
+FROM folio_feesfines.feefineactions__t AS ffa
+LEFT JOIN folio_feesfines.accounts__t AS a ON a.id = ffa.account_id
 LEFT JOIN folio_users.users AS u ON ffa.user_id = u.id
-LEFT JOIN primary_address AS pa ON u.id = pa.user_id
 LEFT JOIN folio_users.groups__t AS pg ON u.patrongroup = pg.id
-LEFT JOIN folio_feesfines.accounts__t AS a ON ffa.account_id = a.id 
-WHERE a.LOCATION NOT LIKE 'Law%'
-AND ffa.source != 'Sierra'
---Enter dates for when the action occurred. 
-AND ffa.date_action BETWEEN '2023-07-07' AND '2023-07-11'
-ORDER BY user_username, item_title, ffa.date_action DESC                                                   
+LEFT JOIN primary_address AS pa ON u.id = pa.user_id
+WHERE ffa.type_action = 'Lost item fee' AND a.fee_fine_type = 'Lost item fee'
+and a.LOCATION NOT LIKE 'Law%'
+and ffa.source != 'Sierra'
+GROUP BY
+ffa.SOURCE,
+a.id,
+ffa.id,
+ffa.date_action,
+a.LOCATION,
+u.jsonb ->> 'barcode',
+u.jsonb ->> 'externalSystemId',
+concat(u.jsonb -> 'personal' ->> 'lastName',', ',u.jsonb -> 'personal' ->> 'firstName'),
+pa.address_line_1,
+CONCAT_WS(' ', CONCAT_WS(', ',pa.address_city, pa.address_region), pa.address_postal_code),
+pg.GROUP,
+a.barcode,
+a.title,
+a.call_number,
+a.amount,
+a.remaining
+ORDER BY concat(u.jsonb -> 'personal' ->> 'lastName',', ',u.jsonb -> 'personal' ->> 'firstName'), a.id, ffa.id DESC 

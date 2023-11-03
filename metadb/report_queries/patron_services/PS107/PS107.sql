571277db424d76fb34d9035ed364b51aebf842ff
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2023-11-03 15:08:23 -0600

    Update PS107.sql
    excludes library department patron group

--- metadb/report_queries/patron_services/PS107/PS107.sql
+++ metadb/report_queries/patron_services/PS107/PS107.sql
@@ -31,6 +31,7 @@ LEFT JOIN primary_address AS pa ON u.id = pa.user_id
 WHERE ffa.type_action = 'Replacement Fee' AND a.fee_fine_type = 'Replacement Fee'
 and a.LOCATION NOT LIKE 'Law%'
 and ffa.source != 'Sierra'
+AND pg.id != 'c4a4cae4-d4a8-5e63-ba36-6b579e4ad5b6'
 --Enter dates in within the green quotations using the format YYYY-MM-DD
 AND ffa.date_action::date BETWEEN '' AND ''
 GROUP BY
@@ -50,4 +51,4 @@ a.title,
 a.call_number,
 a.amount,
 a.remaining
-ORDER BY concat(u.jsonb -> 'personal' ->> 'lastName',', ',u.jsonb -> 'personal' ->> 'firstName'), a.id, ffa.id DESC
+ORDER BY concat(u.jsonb -> 'personal' ->> 'lastName',', ',u.jsonb -> 'personal' ->> 'firstName'), a.id, ffa.id DESC  

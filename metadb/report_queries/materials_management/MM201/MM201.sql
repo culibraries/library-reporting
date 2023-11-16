986ec7bd5fe4f8259df314483bf63a6868aa1abd
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2023-11-16 13:42:59 -0700

    Update MM201.sql
    -This query solves the redundancy and is not missing items. 
    -Uses item status from circulation loan__ & loan__t__ tables.
    -Item status from inventory item__ table causes redundancy, so is not used here.
    -Neither the inventory item__, circulation loan__, nor circulation loan__t__ item status fields produce the CURRENT item status, so this includes current and prior claimed items.

--- metadb/report_queries/materials_management/MM201/MM201.sql
+++ metadb/report_queries/materials_management/MM201/MM201.sql
@@ -3,6 +3,7 @@
 DERIVED TABLES
 
 TABLES
+folio_circulation.loan__
 folio_circulation.loan__t__
 folio_inventory.item__t__
 folio_inventory.location__t__
@@ -13,23 +14,29 @@ FILTERS FOR USER TO SELECT:
 */
 --Returns a list of all items marked 'Claimed returned' across selected Business Library locations.
 SELECT DISTINCT
-	lt.item_status AS status,
+	lsc.id AS loan_id,
+	lsc.jsonb -> 'itemStatus' AS lsc_status,
+	lt.item_status AS lt_status,
 	lt.claimed_returned_date AS claim_date,
 	it.barcode AS barcode,
 	lt2.name AS location,
 	hrt.call_number AS call_number,
-	it.copy_number AS copy_number,
-	it.volume AS volume,
+	it.copy_number AS copy,
+	it.volume AS vol,
 	it2.title AS title,
-	lt.action_comment AS note,
+	lt.action_comment AS claim_note,
 	lt.user_id AS patron_uuid
-FROM folio_circulation.loan__t__ lt 
+FROM folio_circulation.loan__ lsc
+LEFT JOIN folio_circulation.loan__t__ lt ON lt.id = lsc.id
 LEFT JOIN folio_inventory.item__t__ it ON it.id = lt.item_id
-LEFT JOIN folio_inventory.location__t__ lt2 ON lt2.id = lt.item_effective_location_id_at_check_out 
+LEFT JOIN folio_inventory.location__t__ lt2 ON lt2.id = lt.item_effective_location_id_at_check_out   
 LEFT JOIN folio_inventory.holdings_record__t__ hrt ON hrt.id = it.holdings_record_id
 LEFT JOIN folio_inventory.instance__t__ it2 ON it2.id = hrt.instance_id
-WHERE lt.item_status = 'Claimed returned'
+WHERE lsc.jsonb ->> 'itemStatus' = 'Claimed returned'
+	AND lt.item_status = 'Claimed returned'
 	AND lt2.code IN ('BUS','BUSRES','BUSPOP','BUSNB','BUSOV','BUSRES','BUSSER','BUSST')
-	AND call_number IS NOT NULL
+	AND hrt.call_number IS NOT null
+	AND lt.claimed_returned_date IS NOT null
+	AND lt.user_id IS NOT null
 ORDER BY claim_date
 ;

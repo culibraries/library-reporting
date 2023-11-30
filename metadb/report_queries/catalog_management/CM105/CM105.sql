7de2a7c21ad91e451d8e4401b2ce5f95312a8e94
Erin Block <85253480+blocke1@users.noreply.github.com>
2023-11-30 15:43:05 -0700

    Update CM105.sql

--- metadb/report_queries/catalog_management/CM105/CM105.sql
+++ metadb/report_queries/catalog_management/CM105/CM105.sql
@@ -6,8 +6,26 @@ TABLES
 
 FILTERS FOR USER TO SELECT:
 */
-SELECT instance_id FROM folio_source_record.marc__t AS marc
-WHERE (marc.field = '035' and marc.sf = 'a' and marc.content like '%spr%')
-INTERSECT
-(SELECT instance_id FROM folio_source_record.marc__t AS marc2
-where (marc2.field = '998' and marc2.sf = 'e' and marc2.content != 'n'));
+select
+string_agg(distinct hrid, ',') as hrid,
+string_agg(distinct identifier, ',') as identifier,
+string_agg(contributor_name, ';') as author,
+string_agg(distinct publication_place, ',') as publication_place,
+string_agg(distinct publication_role, ',') as publication_role,
+string_agg(distinct publisher, ',') as publisher,
+string_agg(distinct date_of_publication, ',') as date_of_publication,
+string_agg(distinct identifier_type_name, ',') as identifier_type_name,
+string_agg(distinct index_title, ',') as title,
+string_agg(distinct uri, ',') as url,
+string_agg(distinct instance_language, ',') as language
+from folio_derived.instance_identifiers as ii
+left join folio_inventory.instance__t as it on it.id = ii.instance_id
+left join folio_derived.instance_contributors as ic on ic.instance_id = ii.instance_id
+left join folio_derived.instance_editions as ie on ie.instance_id = ic.instance_id
+left join folio_derived.instance_publication as ip on ip.instance_id = ii.instance_id
+left join folio_derived.instance_languages as il on il.instance_hrid = ip.instance_hrid
+left join folio_derived.instance_electronic_access as iea on iea.instance_id = il.instance_id
+where identifier_type_name = 'OCLC'
+and identifier like '%spr%'
+and it.discovery_suppress = false
+group by it.hrid, ii.identifier;

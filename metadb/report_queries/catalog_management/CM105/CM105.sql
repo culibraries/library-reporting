176cccc44bfe696138adf4a5de4013a79355dcd0
Erin Block <85253480+blocke1@users.noreply.github.com>
2024-01-31 16:26:38 -0700

    Update CM105.sql
    Update to Occam's Reader query

--- metadb/report_queries/catalog_management/CM105/CM105.sql
+++ metadb/report_queries/catalog_management/CM105/CM105.sql
@@ -6,26 +6,111 @@ TABLES
 
 FILTERS FOR USER TO SELECT:
 */
-select
-string_agg(distinct hrid, ',') as hrid,
-string_agg(distinct identifier, ',') as identifier,
-string_agg(contributor_name, ';') as author,
-string_agg(distinct publication_place, ',') as publication_place,
-string_agg(distinct publication_role, ',') as publication_role,
-string_agg(distinct publisher, ',') as publisher,
-string_agg(distinct date_of_publication, ',') as date_of_publication,
-string_agg(distinct identifier_type_name, ',') as identifier_type_name,
-string_agg(distinct index_title, ',') as title,
-string_agg(distinct uri, ',') as url,
-string_agg(distinct instance_language, ',') as language
+WITH instance AS (
+WITH
+first_author AS (
+SELECT * from folio_derived.instance_contributors as ic
+where contributor_ordinality = 1
+ORDER BY instance_hrid ASC
+),
+first_edition AS (
+SELECT * from folio_derived.instance_editions as ie
+where edition_ordinality = 1
+ORDER BY instance_hrid ASC
+),
+first_publication AS (
+SELECT * from folio_derived.instance_publication as ip
+where publication_ordinality = 1
+ORDER BY instance_hrid ASC
+),
+first_language AS (
+SELECT * from folio_derived.instance_languages as il
+where il.language_ordinality = 1
+ORDER BY instance_hrid ASC
+),
+electronic_access AS (
+SELECT * from folio_derived.instance_electronic_access AS ea
+where ea.electronic_access_ordinality = 1
+ORDER BY instance_hrid ASC
+),
+print_ISBN AS (
+SELECT *
+FROM folio_source_record.marc__t AS marc
+WHERE marc.field = '020'
+AND marc.sf = 'z'
+AND ord = '1'
+),
+e_ISBN AS (
+SELECT *
+FROM folio_source_record.marc__t AS marc
+WHERE marc.field = '020'
+AND marc.sf = 'a'
+AND ord = '1'
+),
+ebook_package AS (
+SELECT *
+FROM folio_source_record.marc__t AS marc
+WHERE marc.field = '956'
+AND marc.sf = 'a'
+AND ord = '1'
+),
+series_title AS (
+SELECT *
+FROM folio_source_record.marc__t AS marc
+WHERE marc.field = '830'
+AND marc.sf = 'a'
+AND ord = '1'
+),
+series_volume_number AS (
+SELECT *
+FROM folio_source_record.marc__t AS marc
+WHERE marc.field = '830'
+AND marc.sf = 'v'
+AND ord = '1'
+)
+SELECT 
+inst.id AS instance_id,
+auth.contributor_name AS author,
+pub.publication_place AS publication_place,
+pub.publisher AS publisher,
+pub.date_of_publication AS copyright,
+lang.instance_language AS language,
+ed.edition AS edition,
+ea.uri AS url,
+ep.CONTENT AS ebook_package,
+pISBN.CONTENT AS print_ISBN,
+eISBN.CONTENT AS e_ISBN,
+st.CONTENT AS series_title,
+svn.CONTENT AS series_volume_number
+FROM folio_inventory.instance__t AS inst
+LEFT JOIN first_author AS auth ON auth.instance_id = inst.id 
+LEFT JOIN first_edition AS ed ON ed.instance_id = inst.id
+LEFT JOIN first_publication AS pub ON pub.instance_id = inst.id
+LEFT JOIN first_language AS lang ON lang.instance_id = inst.id
+LEFT JOIN electronic_access AS ea ON ea.instance_id = inst.id
+LEFT JOIN print_ISBN AS pISBN ON pISBN.instance_id = inst.id
+LEFT JOIN e_ISBN AS eISBN ON eISBN.instance_id = inst.id
+LEFT JOIN ebook_package AS ep ON ep.instance_id = inst.id
+LEFT JOIN series_title AS st ON st.instance_id = inst.id
+LEFT JOIN series_volume_number AS svn ON svn.instance_id = inst.id
+)
+SELECT 
+II.instance_hrid,
+ii.identifier AS oclc_identifier,
+INSTANCE.author,
+INSTANCE.publication_place,
+INSTANCE.publisher,
+INSTANCE.copyright,
+INSTANCE.edition,
+INSTANCE.LANGUAGE,
+INSTANCE.url,
+INSTANCE.ebook_package,
+INSTANCE.print_ISBN,
+INSTANCE.e_ISBN,
+INSTANCE.series_title,
+INSTANCE.series_volume_number
 from folio_derived.instance_identifiers as ii
-left join folio_inventory.instance__t as it on it.id = ii.instance_id
-left join folio_derived.instance_contributors as ic on ic.instance_id = ii.instance_id
-left join folio_derived.instance_editions as ie on ie.instance_id = ic.instance_id
-left join folio_derived.instance_publication as ip on ip.instance_id = ii.instance_id
-left join folio_derived.instance_languages as il on il.instance_hrid = ip.instance_hrid
-left join folio_derived.instance_electronic_access as iea on iea.instance_id = il.instance_id
+LEFT JOIN INSTANCE ON INSTANCE.INSTANCE_id = ii.instance_id 
 where identifier_type_name = 'OCLC'
 and identifier like '%spr%'
-and it.discovery_suppress = false
-group by it.hrid, ii.identifier;
+ORDER BY instance_hrid ASC

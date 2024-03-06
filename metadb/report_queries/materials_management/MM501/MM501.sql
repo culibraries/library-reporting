4641671075c31882353844f7a59d18aefd597475
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2024-03-06 14:27:11 -0700

    Update MM501.sql
    -Updated and reduced tables, pull entirely through jsonb.
    -CURRENT ITEM STATUS comes from folio_inventory.item__.jsonb -> 'status' BUT STILL GETTING HISTORICAL DATA!
    -For Library Location you can also use: AND loclibrary__.__id IN ('4')
    -No longer using item locations bc are more numerous and will change, but using the broader library locations will ensure everything is captured, and we can see items that should not have been claimed. Seems to work better. Previously used: AND location__.jsonb ->> 'code' IN ('MUS','MUSST','MUSRES','MUSCSA','MUSCSOV','MUSMIN','MUSOV','MUSREF')
    -This query solves redundancy and is not missing items. Linking to a __t table will cause redundancy. Patron emails will cause redundancy.

--- metadb/report_queries/materials_management/MM501/MM501.sql
+++ metadb/report_queries/materials_management/MM501/MM501.sql
@@ -1,42 +1,35 @@
 /** Documentation of Materials Management: Music Claims Returned
 
-DERIVED TABLES
+Returns a list of all items marked 'Claimed returned' across Music Library.
 
 TABLES
 folio_circulation.loan__
-folio_circulation.loan__t__
-folio_inventory.item__t__
-folio_inventory.location__t__
-folio_inventory.holdings_record__t__
-folio_inventory.instance__t__
-
-FILTERS FOR USER TO SELECT:
+folio_inventory.item__
+folio_inventory.location__
+folio_inventory.loclibrary__
 */
---Returns a list of all items marked 'Claimed returned' across selected Music Library locations.
+
 SELECT DISTINCT
-	lsc.id AS loan_id,
-	lsc.jsonb -> 'itemStatus' AS lsc_status,
-	lt.item_status AS lt_status,
-	lt.claimed_returned_date AS claim_date,
-	it.barcode AS barcode,
-	lt2.name AS location,
-	hrt.call_number AS call_number,
-	it.copy_number AS copy,
-	it.volume AS vol,
-	it2.title AS title,
-	lt.action_comment AS claim_note,
-	lt.user_id AS patron_uuid
-FROM folio_circulation.loan__ lsc
-LEFT JOIN folio_circulation.loan__t__ lt ON lt.id = lsc.id
-LEFT JOIN folio_inventory.item__t__ it ON it.id = lt.item_id
-LEFT JOIN folio_inventory.location__t__ lt2 ON lt2.id = lt.item_effective_location_id_at_check_out   
-LEFT JOIN folio_inventory.holdings_record__t__ hrt ON hrt.id = it.holdings_record_id
-LEFT JOIN folio_inventory.instance__t__ it2 ON it2.id = hrt.instance_id
-WHERE lsc.jsonb ->> 'itemStatus' = 'Claimed returned'
-	AND lt.item_status = 'Claimed returned'
-	AND lt2.code IN ('MUS','MUSST','MUSRES','MUSCSA','MUSCSOV','MUSMIN','MUSOV','MUSREF')
-	AND hrt.call_number IS NOT null
-	AND lt.claimed_returned_date IS NOT null
-	AND lt.user_id IS NOT null
-ORDER BY claim_date
+	l.jsonb -> 'id' AS loan_id,
+	i.jsonb -> 'status' ->> 'name' AS status,
+	l.jsonb -> 'claimedReturnedDate' AS clm_date,
+	i.jsonb -> 'barcode' AS barcode,
+	l2.jsonb -> 'name' AS location,
+	i.jsonb -> 'effectiveCallNumberComponents' ->> 'callNumber' AS call_num,
+	i.jsonb -> 'copyNumber' AS cpy,
+	i.jsonb -> 'volume' AS vol,
+	l.jsonb -> 'actionComment' as clm_note,
+	l.jsonb -> 'userId' AS pat_uuid
+FROM folio_circulation.loan__ l
+LEFT JOIN folio_inventory.item__ i on i.jsonb -> 'id' = l.jsonb -> 'itemId'
+LEFT JOIN folio_inventory.location__ l2  on l2.jsonb -> 'id' = i.jsonb -> 'effectiveLocationId'
+LEFT JOIN folio_inventory.loclibrary__ l3 on l3.jsonb -> 'id' = l2.jsonb -> 'libraryId'
+where i.jsonb -> 'status' ->> 'name' = 'Claimed returned'
+	AND l.jsonb ->> 'itemStatus' = 'Claimed returned'
+	AND l3.jsonb ->> 'code' in ('MUS')
+	and l.jsonb ->> 'action' = 'claimedReturned'
+	--AND i.jsonb -> 'effectiveCallNumberComponents' ->> 'callNumber' IS NOT null
+	AND l.jsonb -> 'claimedReturnedDate' IS NOT null
+	AND l.jsonb -> 'userId' IS NOT null
+ORDER BY clm_date
 ;

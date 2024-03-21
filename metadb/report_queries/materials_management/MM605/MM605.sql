4acbeedb38c3915375c232888e37285c43c7913d
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2024-03-20 18:39:36 -0600

    Update MM605.sql
    -Updated tables and eliminated duplication.
    -For Library Location you can also use: AND loclibrary.__id IN ('2','5')
    -No longer using item locations bc are more numerous and will change, but using the broader library locations will ensure everything is captured, and we can see items that should not have been claimed. Seems to work better.
    -Previously used: AND location.jsonb ->> 'code' IN ('ART','ARTST','ARTOV','ARTREF','ASN','ASNST','ASNOV','ASNCIRREF','CYA','CYAST','NORGEN','NORRES','NORGSC','NORDC','NORED','NORRAD','NORDIS','NORDVD','NOROV','NORST','PASCAL','PEROV','PERST','NORPOP','REFCIR','SCIST','SCIOV','SCIREF')
    -Patron emails will cause duplications.
    -Removed --AND i.jsonb -> 'effectiveCallNumberComponents' ->> 'callNumber' IS NOT null-- because eliminated 3D Tech that has data in volume but not call number.

--- metadb/report_queries/materials_management/MM605/MM605.sql
+++ metadb/report_queries/materials_management/MM605/MM605.sql
@@ -3,32 +3,34 @@
 Returns a list of all items marked 'Claimed returned' across Norlin Library and Pascal.
 
 TABLES
-folio_circulation.loan__
-folio_inventory.item__
-folio_inventory.location__
-folio_inventory.loclibrary__
+folio_circulation.loan
+folio_inventory.item
+folio_inventory.item__t
+folio_inventory.location
+folio_inventory.loclibrary
 */
 
-SELECT DISTINCT
+SELECT 
 	l.jsonb -> 'id' AS loan_id,
 	i.jsonb -> 'status' ->> 'name' AS status,
 	l.jsonb -> 'claimedReturnedDate' AS clm_date,
-	i.jsonb -> 'barcode' AS barcode,
+	it.barcode AS barcode,
 	l2.jsonb -> 'name' AS location,
 	i.jsonb -> 'effectiveCallNumberComponents' ->> 'callNumber' AS call_num,
 	i.jsonb -> 'copyNumber' AS cpy,
 	i.jsonb -> 'volume' AS vol,
 	l.jsonb -> 'actionComment' as clm_note,
 	l.jsonb -> 'userId' AS pat_uuid
-FROM folio_circulation.loan__ l
-LEFT JOIN folio_inventory.item__ i on i.jsonb -> 'id' = l.jsonb -> 'itemId'
-LEFT JOIN folio_inventory.location__ l2  on l2.jsonb -> 'id' = i.jsonb -> 'effectiveLocationId'
-LEFT JOIN folio_inventory.loclibrary__ l3 on l3.jsonb -> 'id' = l2.jsonb -> 'libraryId'
+FROM folio_circulation.loan l 
+LEFT JOIN folio_inventory.item i on i.jsonb -> 'id' = l.jsonb -> 'itemId'
+LEFT JOIN folio_inventory.item__t it on it.id = i.id
+LEFT JOIN folio_inventory."location" l2 on l2.jsonb -> 'id' = i.jsonb -> 'effectiveLocationId'
+LEFT JOIN folio_inventory.loclibrary l3 on l3.jsonb -> 'id' = l2.jsonb -> 'libraryId'
 where i.jsonb -> 'status' ->> 'name' = 'Claimed returned'
 	AND l.jsonb ->> 'itemStatus' = 'Claimed returned'
-	AND l3.jsonb ->> 'code' in ('NOR','OFF')  --('NOR','OFF') --('BUS','MUS','ESC','ENG') --('NOR','BUS','MUS','OFF','ESC','ENG')
-	and l.jsonb ->> 'action' = 'claimedReturned'
-	AND l.jsonb -> 'claimedReturnedDate' IS NOT null
-	AND l.jsonb -> 'userId' IS NOT null
+	AND l3.jsonb ->> 'code' in ('NOR','OFF')
+	AND l.jsonb ->> 'action' = 'claimedReturned'
+	AND i."__current" = true
+	AND l."__current" = true
 ORDER BY clm_date
 ;

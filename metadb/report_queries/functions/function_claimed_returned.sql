5eb3487c0876a6af2910af2cef3b6fc981a9ab63
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2024-06-04 14:59:13 -0600

    Create function_claimed_returned.sql
    first draft of claimed_returned function

---
+++ metadb/report_queries/functions/function_claimed_returned.sql
@@ -0,0 +1,45 @@
+--metadb:function claimed_returned
+
+drop function if exists claimed_returned;
+
+create function claimed_returned()
+returns table(
+loan_id text,
+claimed_date date,
+action text,
+action_comment text,
+user_id text,
+user_name text,
+library text,
+item_location text,
+item_barcode text,
+call_number text,
+copy text,
+volume text
+)
+as $$
+select
+l.jsonb ->> 'id' as loan_id,
+l.creation_date::date  as claimed_date,
+l.jsonb ->> 'action' as action,
+l.jsonb ->> 'actionComment' as action_comment,
+l.jsonb ->> 'userId' as user_id,
+concat(u.jsonb -> 'personal' ->> 'firstName',' ', u.jsonb -> 'personal' ->> 'lastName') AS user_name,
+lib.name as library, 
+loc.name as item_location,
+i.jsonb ->> 'barcode' as item_barcode,
+holdings.call_number as call_number,
+i.jsonb ->> 'copyNumber' as copy,
+i.jsonb ->> 'volume' as volume
+from folio_circulation.loan as l
+left join folio_inventory.item as i on i.id = (l.jsonb ->> 'itemId')::uuid
+left join folio_users.users as u on u.id = (l.jsonb ->> 'userId')::uuid
+left join folio_inventory.location__t as loc on loc.id = (i.jsonb ->> 'effectiveLocationId')::uuid
+left join folio_inventory.loclibrary__t as lib on lib.id = loc.library_id
+left join folio_inventory.holdings_record__t as holdings on holdings.id = i.holdingsrecordid 
+where l.jsonb ->> 'action' = 'claimedReturned'
+order by lib.name, l.creation_date::date ASC
+$$
+language sql
+stable 
+parallel safe;

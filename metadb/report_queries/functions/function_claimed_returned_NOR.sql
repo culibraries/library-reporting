ff222f1fdec81c725fdf7a4e0481d075628d1684
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2024-07-10 18:47:43 -0600

    Update function_claimed_returned_NOR.sql

--- metadb/report_queries/functions/function_claimed_returned_NOR.sql
+++ metadb/report_queries/functions/function_claimed_returned_NOR.sql
@@ -39,7 +39,7 @@ left join folio_inventory.loclibrary__t as lib on lib.id = loc.library_id
 left join folio_inventory.holdings_record__t as holdings on holdings.id = i.holdingsrecordid 
 where l.jsonb ->> 'action' = 'claimedReturned'
 	and lib.jsonb ->> 'code' in ('NOR','OFF')
-order by claimed_date asc, loc.name
+order by claimed_date asc
 $$
 language sql
 stable 

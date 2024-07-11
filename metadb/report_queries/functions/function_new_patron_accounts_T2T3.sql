148b2b285e9a1642848902a15c2a6767bff0bc02
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2024-07-10 20:16:29 -0600

    Create function_new_patron_accounts_T2T3.sql

---
+++ metadb/report_queries/functions/function_new_patron_accounts_T2T3.sql
@@ -0,0 +1,45 @@
+--Returns a list of users from Tiers 2 and 3 that were created within the selected date range. Excludes Law patron groups.
+--metadb:function new_patron_accounts_T2T3
+
+drop function if exists new_patron_accounts_T2T3;
+
+create function new_patron_accounts_T2T3(
+  start_date date DEFAULT '2000-01-01',
+  end_date date DEFAULT '2099-01-01'
+)
+returns table(
+created_date date,
+patron_group text,
+username text,
+patron_name text,
+user_id text,
+creator_name text,
+created_by_id text
+)
+as $$
+with creators as (
+	select concat(u.jsonb -> 'personal' ->> 'firstName',' ', u.jsonb -> 'personal' ->> 'lastName') AS creator_name,
+		u.id as created_by_id
+	from folio_users.users u 
+)
+select
+	(u.jsonb -> 'metadata'->>'createdDate')::date as created_date,
+	g.jsonb ->> 'group' as patron_group,
+	u.jsonb ->> 'username' as username,
+	concat(u.jsonb -> 'personal' ->> 'firstName',' ', u.jsonb -> 'personal' ->> 'lastName') AS patron_name,
+	u.jsonb ->> 'id' as user_id,
+	c.creator_name,
+	u.created_by as created_by_id
+from folio_users.users u 
+join creators c on c.created_by_id = u.created_by
+LEFT JOIN folio_users."groups" g  ON u.patrongroup = g.id
+--Enter dates using the format YYYY-MM-DD
+--where u.creation_date ::date BETWEEN '2023-7-01' AND '2024-6-28'
+where g.__id in ('1','2','9','11','13','14','15','23','24','25','26')
+	and u.jsonb -> 'active' = 'true'
+	and u.__current = true
+ORDER BY u.creation_date
+$$
+language sql
+stable 
+parallel safe;

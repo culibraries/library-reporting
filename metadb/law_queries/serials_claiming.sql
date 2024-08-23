56c424c019db3981e62f02ca257dcf149298db11
Rebecca <83369766+rebeccacolaw@users.noreply.github.com>
2024-08-23 08:30:01 -0600

    Create serials_claiming.sql

---
+++ metadb/law_queries/serials_claiming.sql
@@ -0,0 +1,13 @@
+-- This query pulls serial pieces we're expecting but haven't gotten yet. --
+-- Alter the pieces.receipt_date as needed. --
+
+select pol.po_line_number, plf.fund_code , pol.title_or_package, pieces.enumeration,
+	pieces.receiving_status, pieces.receipt_date 
+from folio_orders.po_line__t as pol
+join folio_orders.pieces__t as pieces on pol.id = pieces.po_line_id 
+join folio_derived.po_lines_fund_distribution_transactions as plf on pol.id = plf.po_line_id 
+where pol.receipt_status = 'Ongoing'
+	and pieces.receiving_status = 'Expected'
+	and plf.fund_code like 'Law%'
+	and pieces.receipt_date <= '2023-07-01'
+;

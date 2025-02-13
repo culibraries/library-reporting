cd016502d73f1bc6edfa8002b47ebcafe35c43ea
Erin Block <85253480+blocke1@users.noreply.github.com>
2025-02-13 15:20:03 -0700

    Update function_gold_rush_holdings.sql

--- metadb/report_queries/functions/function_gold_rush_holdings.sql
+++ metadb/report_queries/functions/function_gold_rush_holdings.sql
@@ -9,4 +9,8 @@ FROM
 folio_inventory.instance__t AS it
 left join folio_derived.holdings_ext as he on he.instance_id = it.id
 where he.permanent_location_name != '%Law%'
-and it.discovery_suppress is not true;
+and it.discovery_suppress is not true
+$$
+language sql 
+stable 
+parallel safe;

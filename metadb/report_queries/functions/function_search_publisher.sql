1a99ea7c2e58ef1f59b82b2117512264cacb4469
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2025-02-18 11:32:44 -0700

    Update function_search_publisher.sql

--- metadb/report_queries/functions/function_search_publisher.sql
+++ metadb/report_queries/functions/function_search_publisher.sql
@@ -28,7 +28,7 @@ left join folio_inventory.instance__t as inst on inst.id = pub.instance_id
 left join folio_inventory.holdings_record__t as ho on ho.instance_id = inst.id
 left join folio_inventory.item__t as i on i.holdings_record_id = ho.id
 left join folio_inventory.location__t as loc on loc.id = i.effective_location_id
-where pub.publisher like Concat('%',search_text,'%')
+where pub.publisher ilike Concat('%',search_text,'%')
 $$
 language sql
 stable parallel SAFE;

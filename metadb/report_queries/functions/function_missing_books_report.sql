f904a1a2155202dcb58c554245b6d63972029107
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2025-03-25 14:58:59 -0600

    Create function_missing_books_report.sql

---
+++ metadb/report_queries/functions/function_missing_books_report.sql
@@ -0,0 +1,38 @@
+--metadb: Function MM102 - Missing Books Report - All Locations
+drop function if exists missing_books_report;
+
+create function missing_books_report()
+returns table(
+tag text,
+item_status text,
+item_location text,
+item_barcode text,
+call_number text,
+shelf_order text,
+title text,
+enumeration text,
+volume text,
+copy_number text
+)
+as $$
+SELECT 
+jsonb_extract_path_text(i.jsonb, 'tags', 'tagList') AS tag,
+i.jsonb -> 'status' ->> 'name' AS item_status,
+loc.name AS item_location,
+i.jsonb ->> 'barcode' AS item_barcode,
+i.jsonb -> 'effectiveCallNumberComponents' ->> 'callNumber' AS call_number,
+i.jsonb ->> 'effectiveShelvingOrder' AS shelf_order,
+inst.title AS title,
+i.jsonb ->> 'enumeration' AS enumeration,
+i.jsonb ->> 'copyNumber' AS volume,
+i.jsonb ->> 'volume' AS copy_number
+FROM folio_inventory.item i
+LEFT JOIN folio_inventory.location__t AS loc ON loc.id = (i.jsonb ->> 'effectiveLocationId')::uuid
+LEFT JOIN folio_inventory.holdings_record__t AS holdings ON holdings.id = i.holdingsrecordid
+LEFT JOIN folio_inventory.instance__t AS inst ON inst.id = holdings.instance_id
+WHERE i.jsonb -> 'status' ->> 'name' = 'Missing'
+ORDER BY loc.name, i.jsonb ->> 'effectiveShelvingOrder' ASC
+$$
+language sql
+stable 
+parallel safe;

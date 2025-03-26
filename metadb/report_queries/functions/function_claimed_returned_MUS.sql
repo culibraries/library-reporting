e985d879fecadf9252f61fb494adddb0713f79da
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2025-03-26 16:12:09 -0600

    Update function_claimed_returned_MUS.sql

--- metadb/report_queries/functions/function_claimed_returned_MUS.sql
+++ metadb/report_queries/functions/function_claimed_returned_MUS.sql
@@ -1,45 +1,45 @@
---metadb:function claimed_returned_MUS
+--metadb:function claimed_returned_MUS --Music Library
 
 drop function if exists claimed_returned_MUS;
 
 create function claimed_returned_MUS()
 returns table(
 claimed_date date,
-"library" text,
-item_location text,
-item_barcode text,
+barcode text,
+location text,
 call_number text,
-"copy" text,
-volume text,
-action_comment text,
+cpy text,
+vol text,
+claim_note text,
+loan_id text,
 user_name text,
 user_email text,
-user_id text,
-loan_id text
+user_id text
 )
 as $$
-select
-(i.jsonb -> 'status' ->> 'date')::DATE as claimed_date,
-lib.name as "library", 
-loc.name as item_location,
-i.jsonb ->> 'barcode' as item_barcode,
-holdings.call_number as call_number,
-i.jsonb ->> 'copyNumber' as "copy",
-i.jsonb ->> 'volume' as volume,
-l.jsonb ->> 'actionComment' as action_comment,
-concat(u.jsonb -> 'personal' ->> 'firstName',' ', u.jsonb -> 'personal' ->> 'lastName') AS user_name,
-u.jsonb -> 'personal' ->> 'email' as user_email,
-l.jsonb ->> 'userId' as user_id,
-l.jsonb ->> 'id' as loan_id
-from folio_circulation.loan as l
-left join folio_inventory.item as i on i.id = (l.jsonb ->> 'itemId')::uuid
-left join folio_users.users as u on u.id = (l.jsonb ->> 'userId')::uuid
-left join folio_inventory.location__t as loc on loc.id = (i.jsonb ->> 'effectiveLocationId')::uuid
-left join folio_inventory.loclibrary__t as lib on lib.id = loc.library_id
-left join folio_inventory.holdings_record__t as holdings on holdings.id = i.holdingsrecordid 
-where l.jsonb ->> 'action' = 'claimedReturned'
-	and lib.code in ('MUS')
-order by claimed_date asc
+SELECT 
+	(i.jsonb -> 'status' ->> 'date')::DATE as claimed_date,
+	i.jsonb ->> 'barcode' as barcode,
+	lt.discovery_display_name as location,
+	holdings.call_number as call_number,
+	i.jsonb ->> 'copyNumber' AS cpy,
+	i.jsonb ->> 'volume' AS vol,
+	l.jsonb ->> 'actionComment' as claim_note,
+	l.jsonb ->> 'id' AS loan_id,
+	concat(u.jsonb -> 'personal' ->> 'firstName',' ', u.jsonb -> 'personal' ->> 'lastName') AS user_name,
+	u.jsonb -> 'personal' ->> 'email' as user_email,
+	l.jsonb ->> 'userId' AS user_id
+FROM folio_circulation.loan l 
+LEFT JOIN folio_inventory.item i on i.id = (l.jsonb ->> 'itemId')::uuid
+LEFT JOIN folio_users.users as u on u.id = (l.jsonb ->> 'userId')::uuid
+LEFT JOIN folio_inventory.location__t lt  on lt.id = (i.jsonb ->> 'effectiveLocationId')::uuid
+LEFT JOIN folio_inventory.loclibrary__t lt2 on lt2.id = lt.library_id 
+LEFT JOIN folio_inventory.holdings_record__t as holdings on holdings.id = i.holdingsrecordid
+where i.jsonb -> 'status' ->> 'name' = 'Claimed returned'
+	AND l.jsonb ->> 'itemStatus' = 'Claimed returned'
+	AND l.jsonb ->> 'action' = 'claimedReturned'
+	and lt2.code in ('MUS') 
+ORDER BY claimed_date asc
 $$
 language sql
 stable 

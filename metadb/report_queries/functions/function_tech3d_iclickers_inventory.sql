527358d71b38cb23e953180291bae5f9aa59973f
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2025-05-29 21:48:19 -0600

    Update function_tech3d_iclickers_inventory.sql

--- metadb/report_queries/functions/function_tech3d_iclickers_inventory.sql
+++ metadb/report_queries/functions/function_tech3d_iclickers_inventory.sql
@@ -4,7 +4,11 @@ drop function if exists tech3d_iclickers_inventory;
 
 create function tech3d_iclickers_inventory(
 location_code1 text,
-location_code2 text
+location_code2 text,
+location_code3 text,
+location_code4 text,
+location_code5 text,
+location_code6 text
 )
 returns table(
 "Status" text,
@@ -25,7 +29,7 @@ select
 from folio_inventory.item i
 left join folio_inventory.location l on (l.jsonb ->> 'id')::uuid = (i.jsonb ->> 'effectiveLocationId')::uuid
 left join folio_inventory.holdings_record hr on (hr.jsonb ->> 'id')::uuid = (i.jsonb ->> 'holdingsRecordId')::uuid
-where l.jsonb ->> 'code' in (location_code1, location_code2)
+where l.jsonb ->> 'code' in (location_code1, location_code2, location_code3, location_code4, location_code5, location_code6)
 and hr.jsonb ->> 'hrid' in ('b12730993-032','b12730993-022','b12730993-009','ho00000214255','b12730993-001','b12730993-013','b10268020-001','ho00000350100')
 order by "Status","Volume"
 $$

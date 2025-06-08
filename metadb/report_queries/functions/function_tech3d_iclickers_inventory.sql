4c56e09b6d7085e0b28f8f87a8a7a11633c86e52
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2025-06-07 20:24:46 -0600

    Update function_tech3d_iclickers_inventory.sql

--- metadb/report_queries/functions/function_tech3d_iclickers_inventory.sql
+++ metadb/report_queries/functions/function_tech3d_iclickers_inventory.sql
@@ -10,7 +10,7 @@ returns table(
 "Status" text,
 "Barcode" text,
 "Volume" text,
-"Updated" timestamp,
+"Updated" date,
 "Location Name" text
 )
 as $$
@@ -23,9 +23,11 @@ select
 from folio_inventory.item i
 left join folio_inventory.location l on (l.jsonb ->> 'id')::uuid = (i.jsonb ->> 'effectiveLocationId')::uuid
 left join folio_inventory.holdings_record hr on (hr.jsonb ->> 'id')::uuid = (i.jsonb ->> 'holdingsRecordId')::uuid
-where l.jsonb ->> 'name' in (location_name1, location_name2)
---where l.jsonb ->> 'code' in (location_code1, location_code2, location_code3, location_code4, location_code5, location_code6)
-and hr.jsonb ->> 'hrid' in ('b12730993-032','b12730993-022','b12730993-009','ho00000214255','b12730993-001','b12730993-013','b10268020-001','ho00000350100')
+where hr.jsonb ->> 'hrid' in ('b12730993-032','b12730993-022','b12730993-009','ho00000214255','b12730993-001','b12730993-013','b10268020-001','ho00000350100')
+and case 
+	when location_name1 = 'ALL' then true
+	else l.jsonb ->> 'name' in (location_name1, location_name2)
+end
 order by "Status","Volume"
 $$
 language sql

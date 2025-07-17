accef9eb0fed77da5d71d7ece3b76ae003ceb409
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2025-07-17 11:22:44 -0600

    Create course_reserves_permloantype.sql

---
+++ metadb/report_queries/functions/course_reserves_permloantype.sql
@@ -0,0 +1,23 @@
+--metadb:function course_reserves_permloantype
+
+drop function if exists course_reserve_permloantype;
+
+create function course_reserve_permloantype()
+returns table(
+"Title" text,
+"Barcode" text,
+"Call Number" text
+)
+as $$
+SELECT 
+	i2.jsonb ->> 'title' as "Title",
+	i.jsonb ->> 'barcode' as "Barcode",
+	i.jsonb -> 'effectiveCallNumberComponents' ->> 'callNumber' AS "Call Number"
+FROM folio_inventory.item i
+left join folio_inventory.instance i2 on i2.jsonb ->> 'hrid' = i.jsonb ->> 'hrid'
+AND i.jsonb ->> 'permanentLoanTypeId' = 'e8b311a6-3b21-43f2-a269-dd9310cb2d0e'
+ORDER BY "Call Number"
+$$
+language sql
+stable 
+parallel safe;

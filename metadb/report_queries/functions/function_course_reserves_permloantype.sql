102056bd550a26ef0ce7d58cdad33eec3f6c58ca
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2025-07-17 12:26:42 -0600

    Update function_course_reserves_permloantype.sql

--- metadb/report_queries/functions/function_course_reserves_permloantype.sql
+++ metadb/report_queries/functions/function_course_reserves_permloantype.sql
@@ -10,12 +10,12 @@ returns table(
 )
 as $$
 SELECT 
-	i2.jsonb ->> 'title' as "Title",
-	i.jsonb ->> 'barcode' as "Barcode",
-	i.jsonb -> 'effectiveCallNumberComponents' ->> 'callNumber' AS "Call Number"
+	(i2.jsonb ->> 'title') as "Title",
+	(i.jsonb ->> 'barcode') as "Barcode",
+	(i.jsonb -> 'effectiveCallNumberComponents' ->> 'callNumber') AS "Call Number"
 FROM folio_inventory.item i
-left join folio_inventory.instance i2 on i2.jsonb ->> 'hrid' = i.jsonb ->> 'hrid'
-AND i.jsonb ->> 'permanentLoanTypeId' = 'e8b311a6-3b21-43f2-a269-dd9310cb2d0e'
+left join folio_inventory.instance i2 on (i2.jsonb ->> 'hrid') = (i.jsonb ->> 'hrid')
+where (i.jsonb ->> 'permanentLoanTypeId') = 'e8b311a6-3b21-43f2-a269-dd9310cb2d0e'
 ORDER BY "Call Number"
 $$
 language sql

18bab258aeb59f553765c2e88e9922b664f2a6ca
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2025-07-17 16:38:23 -0600

    Create function_paid_fines_individual.sql

---
+++ metadb/report_queries/functions/function_paid_fines_individual.sql
@@ -0,0 +1,41 @@
+--metadb:function paid_fines_individual
+
+drop function if exists paid_fines_individual;
+
+create function paid_fines_individual(
+	start_date date DEFAULT '2000-01-01',
+ 	end_date date DEFAULT '2999-01-01'
+)
+returns table(
+"Patron Name" text,
+"Ext Sys ID" text,
+"Date Paid" date,
+"Fine Type" text,
+"Paid Amount" text,
+"Material Type" text,
+"Barcode" text,
+"Call Number" text,
+"Volume" text
+)
+as $$
+SELECT DISTINCT
+	CONCAT(u.jsonb -> 'personal' ->> 'firstName',' ', u.jsonb -> 'personal' ->> 'lastName') AS "Patron Name",
+	u.jsonb ->> 'externalSystemId' AS "Ext Sys ID",
+	(a.jsonb -> 'metadata' ->> 'updatedDate')::date AS "Date Paid", 
+	a.jsonb ->> 'feeFineType' AS "Fine Type",
+	a.jsonb ->> 'amount' AS "Paid Amount",
+	a.jsonb ->> 'materialType' AS "Material Type",
+	a.jsonb ->> 'barcode' AS "Barcode",
+	a.jsonb ->> 'callNumber' AS "Call Number",
+	i.jsonb ->> 'volume' AS "Volume"
+FROM folio_feesfines.accounts a
+LEFT JOIN folio_inventory.item i ON i.id = (a.jsonb ->> 'itemId')::uuid
+LEFT JOIN folio_users.users u ON u.id = (a.jsonb ->> 'userId')::uuid
+WHERE a.jsonb -> 'paymentStatus' ->> 'name' = 'Paid fully'
+	AND (a.jsonb -> 'metadata' ->> 'updatedDate')::date BETWEEN '2024-7-01' AND '2025-6-30'
+	AND a.jsonb ->> 'feeFineOwner' IN ('Patron Accounts','University Libraries')
+ORDER BY "Date Paid" ASC
+$$
+language sql
+stable 
+parallel safe;

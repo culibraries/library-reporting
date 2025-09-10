4f1dc7f56e25f8280d5038b7f2c7375bcd215ea4
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2025-09-10 10:58:45 -0600

    Update function_waived_fines_individual.sql

--- metadb/report_queries/functions/function_waived_fines_individual.sql
+++ metadb/report_queries/functions/function_waived_fines_individual.sql
@@ -4,7 +4,8 @@ drop function if exists waived_fines_individual;
 
 create function waived_fines_individual(
 	start_date date DEFAULT '2000-01-01',
- 	end_date date DEFAULT '2999-01-01'
+ 	end_date date DEFAULT '2999-01-01',
+ 	material_type_name_search text default ''
 )
 returns table(
 "Patron Name" text,
@@ -31,7 +32,9 @@ SELECT DISTINCT
 FROM folio_feesfines.accounts a
 LEFT JOIN folio_inventory.item i ON i.id = (a.jsonb ->> 'itemId')::uuid
 LEFT JOIN folio_users.users u ON u.id = (a.jsonb ->> 'userId')::uuid
-WHERE a.jsonb -> 'paymentStatus' ->> 'name' = 'Waived fully'
+WHERE a.jsonb ->> 'materialType' ilike Concat('%',material_type_name_search,'%')
+	--AND a.jsonb ->> 'materialType' IN (material_type_name)
+	and a.jsonb -> 'paymentStatus' ->> 'name' = 'Waived fully'
 	and start_date <= (a.jsonb -> 'metadata' ->> 'updatedDate')::date and end_date >= (a.jsonb -> 'metadata' ->> 'updatedDate')::date
 	--AND (a.jsonb -> 'metadata' ->> 'updatedDate')::date BETWEEN '2024-7-01' AND '2025-6-30'
 	AND a.jsonb ->> 'feeFineOwner' IN ('Patron Accounts','University Libraries')

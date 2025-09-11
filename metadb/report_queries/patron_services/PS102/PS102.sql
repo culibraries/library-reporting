57c172d39508ba27aac59d5fc05704295cf70196
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2025-09-11 14:26:40 -0600

    Update PS102.sql

--- metadb/report_queries/patron_services/PS102/PS102.sql
+++ metadb/report_queries/patron_services/PS102/PS102.sql
@@ -1,37 +1,36 @@
 /** Documentation of Patron Services: Claimed Returned Report 
 
-Returns a list of all items marked 'Claimed returned' across all library locations and PASCAL offsite.
+Returns a list of all items marked 'Claimed returned' across all library locations and Pascal.
 
 TABLES
 folio_circulation.loan
 folio_inventory.item
 folio_users.users
-folio_inventory.location__t
-folio_inventory.loclibrary__t
-folio_inventory.holdings_record__t
+folio_inventory.location
+folio_inventory.loclibrary
 */
 
 SELECT 
-	(i.jsonb -> 'status' ->> 'date')::DATE as claimed_date,
-	i.jsonb ->> 'barcode' as barcode,
-	lt.discovery_display_name as location,
-	holdings.call_number as call_number,
-	i.jsonb ->> 'copyNumber' AS cpy,
-	i.jsonb ->> 'volume' AS vol,
-	l.jsonb ->> 'actionComment' as claim_note,
-	l.jsonb ->> 'id' AS loan_id,
-	concat(u.jsonb -> 'personal' ->> 'firstName',' ', u.jsonb -> 'personal' ->> 'lastName') AS user_name,
-	u.jsonb -> 'personal' ->> 'email' as user_email,
-	l.jsonb ->> 'userId' AS user_id
+	(l.jsonb ->> 'claimedReturnedDate')::date AS "Claimed Date",
+	i.jsonb ->> 'barcode' as "Barcode",
+	l2.jsonb ->> 'name' AS "Location Name",
+	i.jsonb -> 'effectiveCallNumberComponents' ->> 'callNumber' AS "Call Number",
+	i.jsonb ->> 'copyNumber' AS "Copy",
+	i.jsonb ->> 'volume' AS "Volume",
+	l.jsonb ->> 'actionComment' as "Claim Note",
+	l.jsonb ->> 'id' AS "Loan ID",
+	concat(u.jsonb -> 'personal' ->> 'firstName',' ', u.jsonb -> 'personal' ->> 'lastName') AS "User Name",
+	u.jsonb -> 'personal' ->> 'email' as "User Email"
 FROM folio_circulation.loan l 
 LEFT JOIN folio_inventory.item i on i.id = (l.jsonb ->> 'itemId')::uuid
 LEFT JOIN folio_users.users as u on u.id = (l.jsonb ->> 'userId')::uuid
-LEFT JOIN folio_inventory.location__t lt  on lt.id = (i.jsonb ->> 'effectiveLocationId')::uuid
-LEFT JOIN folio_inventory.loclibrary__t lt2 on lt2.id = lt.library_id 
-LEFT JOIN folio_inventory.holdings_record__t as holdings on holdings.id = i.holdingsrecordid
-where i.jsonb -> 'status' ->> 'name' = 'Claimed returned'
-	AND l.jsonb ->> 'itemStatus' = 'Claimed returned'
-	AND l.jsonb ->> 'action' = 'claimedReturned'
-	and lt2.code in ('NOR','BUS','MUS','OFF','ESC','ENG') 
-ORDER BY claimed_date asc
+LEFT JOIN folio_inventory.location l2 on l2.id = (i.jsonb ->> 'effectiveLocationId')::uuid
+LEFT JOIN folio_inventory.loclibrary l3 on l3.id = (l2.jsonb ->> 'libraryId')::uuid
+WHERE l3.jsonb ->> 'code' in ('NOR','BUS','MUS','OFF','ESC','ENG','INN-REACH')
+	AND i.jsonb -> 'status' ->> 'name' = 'Claimed returned'
+	AND l.jsonb ->> 'action' = 'claimedReturned' 
+ORDER BY "Claimed Date" asc, "Location Name"
 ;
+
+--Location names from L3 LocLibrary: Norlin, Business, Music, Offsite, INN-REACH, Earth Sciences & Map, Engineering Math & Physics
+--Location codes: NOR, BUS, MUS, OFF, INN-REACH, ESC, ENG

66e585686176296edf15d3143d390fd1cdc812d3
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2025-09-19 14:42:06 -0600

    Update function_paid_fines_individual.sql

--- metadb/report_queries/functions/function_paid_fines_individual.sql
+++ metadb/report_queries/functions/function_paid_fines_individual.sql
@@ -13,6 +13,7 @@ returns table(
 "Date Paid" date,
 "Fine Type" text,
 "Paid Amount" text,
+"Status" text,
 "Material Type" text,
 "Barcode" text,
 "Call Number" text,
@@ -25,6 +26,7 @@ SELECT DISTINCT
 	ffa.date_action::date AS "Date Paid",
 	a.jsonb ->> 'feeFineType' AS "Fine Type",
 	a.jsonb ->> 'amount' AS "Paid Amount",
+	a.jsonb -> 'paymentStatus' ->> 'name' AS "Status",
 	a.jsonb ->> 'materialType' AS "Material Type",
 	a.jsonb ->> 'barcode' AS "Barcode",
 	a.jsonb ->> 'callNumber' AS "Call Number",
@@ -33,12 +35,11 @@ FROM folio_feesfines.accounts a
 LEFT JOIN folio_inventory.item i ON i.id = (a.jsonb ->> 'itemId')::uuid
 LEFT JOIN folio_users.users u ON u.id = (a.jsonb ->> 'userId')::uuid
 LEFT JOIN folio_feesfines.feefineactions__t ffa on ffa.account_id = a.id
-WHERE a.jsonb ->> 'materialType' ilike Concat('%',material_type_name_search,'%')
-	--AND a.jsonb ->> 'materialType' IN (material_type_name)
-	AND a.jsonb -> 'paymentStatus' ->> 'name' = 'Paid fully'
-	AND start_date <= (a.jsonb -> 'metadata' ->> 'updatedDate')::date and end_date >= (a.jsonb -> 'metadata' ->> 'updatedDate')::date
+WHERE start_date <= (a.jsonb -> 'metadata' ->> 'updatedDate')::date and end_date >= (a.jsonb -> 'metadata' ->> 'updatedDate')::date
+	AND a.jsonb ->> 'materialType' ilike Concat('%',material_type_name_search,'%')
+	AND a.jsonb -> 'paymentStatus' ->> 'name' in ('Paid fully','Paid partially') 
 	AND a.jsonb ->> 'feeFineOwner' IN ('Patron Accounts','University Libraries')
-	AND ffa.type_action in ('Paid fully','Paid partially')
+	--AND ffa.type_action in ('Paid fully','Paid partially')
 ORDER BY "Date Paid" ASC
 $$
 language sql

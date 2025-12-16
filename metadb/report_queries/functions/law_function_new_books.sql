a9e2d155d691a836acf27c13483849c22291ae8a
Rebecca <83369766+rebeccacolaw@users.noreply.github.com>
2025-12-16 13:58:43 -0700

    Update law_function_new_books.sql

--- metadb/report_queries/functions/law_function_new_books.sql
+++ metadb/report_queries/functions/law_function_new_books.sql
@@ -6,28 +6,27 @@ CREATE FUNCTION law_new_books(
     start_date date DEFAULT '2025-01-01'
     )
 RETURNS TABLE(
-  hrid,
-  title,
-  call_num,
-  location,
-  isbn,
-  cat_date,
-  requestor
+    hrid,i.title,
+    h.call_number,
+    loc."name",
+    folio_source_record.marc__t."content",
+    i.cataloged_date,pol.requester
   )
 AS $$
-select folio_inventory.instance__t.hrid as hrid,
-    folio_inventory.instance__t.title as title, folio_inventory.holdings_record__t.call_number as call_num, 
-    folio_inventory.location__t."name" as location, folio_source_record.marc__t."content" as isbn,
-    folio_inventory.instance__t.cataloged_date as cat_date,
-    folio_orders.po_line__t.requester as requestor
-from folio_inventory.instance__t
-join folio_inventory.holdings_record__t on folio_inventory.instance__t.id = folio_inventory.holdings_record__t.instance_id
-join folio_inventory.location__t on folio_inventory.holdings_record__t.permanent_location_id = folio_inventory.location__t.id
+select i.hrid,
+    i.title, h.call_number, 
+    loc."name",
+    folio_source_record.marc__t."content",
+    i.cataloged_date,
+    pol.requester
+from folio_inventory.instance__t as i
+join folio_inventory.holdings_record__t as h on folio_inventory.instance__t.id = folio_inventory.holdings_record__t.instance_id
+join folio_inventory.location__t as loc on folio_inventory.holdings_record__t.permanent_location_id = folio_inventory.location__t.id
 join folio_source_record.marc__t on folio_inventory.instance__t.id = folio_source_record.marc__t.instance_id
-join folio_orders.po_line__t on folio_inventory.instance__t.id = folio_orders.po_line__t.instance_id
-where folio_inventory.instance__t.cataloged_date > start_date
-    and folio_inventory.location__t."name" like '%Law%'
-    and folio_inventory.location__t."name" != 'Law Electronic Resources'
+join folio_orders.po_line__t as pol on folio_inventory.instance__t.id = folio_orders.po_line__t.instance_id
+where i.cataloged_date > start_date
+    and loc."name" like '%Law%'
+    and loc."name" != 'Law Electronic Resources'
     and folio_source_record.marc__t.field = '020'
 $$
 LANGUAGE SQL

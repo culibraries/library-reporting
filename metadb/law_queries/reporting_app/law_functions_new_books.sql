6527446c20e8e27fae4e3f3446336a83fbf7e7c6
Rebecca <83369766+rebeccacolaw@users.noreply.github.com>
2025-12-16 13:30:12 -0700

    Update law_functions_new_books.sql

--- metadb/law_queries/reporting_app/law_functions_new_books.sql
+++ metadb/law_queries/reporting_app/law_functions_new_books.sql
@@ -15,7 +15,7 @@ RETURNS TABLE(
   requestor
   )
 AS $$
-select distinct on (folio_inventory.instance__t.hrid) folio_inventory.instance__t.hrid as hrid,
+select folio_inventory.instance__t.hrid as hrid,
   folio_inventory.instance__t.title as title, folio_inventory.holdings_record__t.call_number as call_num, 
   folio_inventory.location__t."name" as location, folio_source_record.marc__t."content" as isbn,
   folio_inventory.instance__t.cataloged_date as cat_date,
@@ -25,8 +25,7 @@ select distinct on (folio_inventory.instance__t.hrid) folio_inventory.instance__
   join folio_inventory.location__t on folio_inventory.holdings_record__t.permanent_location_id = folio_inventory.location__t.id
   join folio_source_record.marc__t on folio_inventory.instance__t.id = folio_source_record.marc__t.instance_id
   join folio_orders.po_line__t on folio_inventory.instance__t.id = folio_orders.po_line__t.instance_id
-  where folio_inventory.instance__t.cataloged_date >= '2023-10-10'
-    and folio_inventory.location__t."name" like '%Law%'
+  where folio_inventory.location__t."name" like '%Law%'
     and folio_inventory.location__t."name" != 'Law Electronic Resources'
     and folio_source_record.marc__t.field = '020'
 $$

9f2eb1c4dbd8d7d2be0912cbc492c0f7c3030e60
Rebecca <83369766+rebeccacolaw@users.noreply.github.com>
2025-12-18 10:17:03 -0700

    Update law_function_new_books.sql

--- metadb/report_queries/functions/law_function_new_books.sql
+++ metadb/report_queries/functions/law_function_new_books.sql
@@ -1,16 +1,18 @@
 --metadb:function law_new_books
 
-DROP FUNCTION IF EXISTS law_new_books;
+DROP FUNCTION IF EXISTS law_new_books(start_date);
 
 CREATE FUNCTION law_new_books(
     start_date date DEFAULT '2025-01-01'
     )
 RETURNS TABLE(
-    hrid,i.title,
-    h.call_number,
-    loc."name",
-    folio_source_record.marc__t."content",
-    i.cataloged_date,pol.requester
+    hrid text,
+    i.title text,
+    h.call_number text,
+    loc."name" text,
+    folio_source_record.marc__t."content" text,
+    i.cataloged_date timestampz,
+    pol.requester
   )
 AS $$
 select i.hrid,
@@ -20,10 +22,10 @@ select i.hrid,
     i.cataloged_date,
     pol.requester
 from folio_inventory.instance__t as i
-join folio_inventory.holdings_record__t as h on folio_inventory.instance__t.id = folio_inventory.holdings_record__t.instance_id
-join folio_inventory.location__t as loc on folio_inventory.holdings_record__t.permanent_location_id = folio_inventory.location__t.id
-join folio_source_record.marc__t on folio_inventory.instance__t.id = folio_source_record.marc__t.instance_id
-join folio_orders.po_line__t as pol on folio_inventory.instance__t.id = folio_orders.po_line__t.instance_id
+join folio_inventory.holdings_record__t as h on i.id = h.instance_id
+join folio_inventory.location__t as loc on h.permanent_location_id = loc.id
+join folio_source_record.marc__t on i.id = folio_source_record.marc__t.instance_id
+join folio_orders.po_line__t as pol on i.id = pol.instance_id
 where i.cataloged_date > start_date
     and loc."name" like '%Law%'
     and loc."name" != 'Law Electronic Resources'

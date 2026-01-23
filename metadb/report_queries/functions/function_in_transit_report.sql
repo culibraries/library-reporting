71aed37d198e61ab36d869f2fe14350dc667e90d
Amelia <109626755+AmeliaDS@users.noreply.github.com>
2026-01-23 14:27:14 -0700

    Update function_in_transit_report.sql

--- metadb/report_queries/functions/function_in_transit_report.sql
+++ metadb/report_queries/functions/function_in_transit_report.sql
@@ -1,13 +1,13 @@
 --metadb:function in_transit_report
 
-drop function if exists in_transit_report;
+DROP FUNCTION IF EXISTS in_transit_report;
 
-create function in_transit_report(
+CREATE FUNCTION in_transit_report(
 	p_lib_location text,
 	start_date date DEFAULT '2000-01-01',
 	end_date date DEFAULT '2999-01-01'
 	)
-returns table(
+RETURNS TABLE(
 "Item hrid" text,
 "Item Created Date" date,
 "Holdings Call Number" text,
@@ -20,16 +20,16 @@ returns table(
 "Effective Location" text,
 "Title" text
 )
-as $$
-with checkin as (
-select 
-	item_id, 
-	MAX(occurred_date_time) as most_recent_time,
-	cit.item_status_prior_to_check_in as prior_status,
-	spt."name" as last_service_point
-from folio_circulation.check_in__t cit
+AS $$
+WITH checkin AS (
+SELECT DISTINCT ON (cit.item_id)
+    cit.item_id,
+    cit.occurred_date_time AS most_recent_time,
+    cit.item_status_prior_to_check_in AS prior_status,
+    spt."name" AS last_service_point
+FROM folio_circulation.check_in__t cit
 LEFT JOIN folio_inventory.service_point__t spt ON spt.id = cit.service_point_id
-group by item_id, prior_status, spt."name"
+ORDER BY cit.item_id, cit.occurred_date_time DESC
 )	
 SELECT
 	i.jsonb->>'hrid' AS "Item hrid", 
@@ -54,8 +54,8 @@ WHERE
 	AND (NULLIF(p_lib_location, '') IS NULL OR llt."name" = p_lib_location)
 	AND (i.creation_date::date) <= start_date	
 	AND (checkin.most_recent_time::date) <= end_date
-ORDER BY "Effective Location" asc,"Last Checkin Date" asc
+ORDER BY "Effective Location" ASC,"Last Checkin Date" ASC
 $$
-language sql
-stable 
-parallel safe;
+LANGUAGE SQL
+STABLE 
+PARALLEL safe;

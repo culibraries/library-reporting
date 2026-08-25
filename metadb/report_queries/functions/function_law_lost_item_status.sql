0cc7627ecd11ab8ce9d9365db1dd8ee341d3b347
Erin Block <85253480+blocke1@users.noreply.github.com>
2026-08-25 15:34:47 -0600

    Update function_law_lost_item_status.sql

--- metadb/report_queries/functions/function_law_lost_item_status.sql
+++ metadb/report_queries/functions/function_law_lost_item_status.sql
@@ -1,22 +1,30 @@
 --metadb:function law_lost_item_status
 
-drop function if exists law_lost_item_status;
+drop function if exists law_lost_item_status(text);
 
 create function law_lost_item_status()
 returns table(
-instance_hrid text  
+hrid text,
+barcode text,
+call_number text,
+material_type text,
+location text,
+status text,
+discovery_suppress text,
+note_type text,
+note text
 )  
 as $$
 select
-ie.item_hrid,
-ie.barcode,
-ie.effective_call_number,
-ie.material_type_name,
-ie.effective_location_name,
-ie.status_name,
-ie.discovery_suppress,
-itn.note_type_name,
-itn.note
+ie.item_hrid as hrid,
+ie.barcode as barcode,
+ie.effective_call_number as call_number,
+ie.material_type_name as material_type,
+ie.effective_location_name as location,
+ie.status_name as status,
+ie.discovery_suppress as discovery_suppress,
+itn.note_type_name as note_type,
+itn.note as note
 from folio_derived.item_ext ie
 left join folio_derived.item_notes as itn on itn.item_id = ie.item_id
 where (ie.status_name = 'Long missing'
